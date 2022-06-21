5cc11c8b4f64c68096343e4aca9c8a6415eb0c66
p_pjiahyan <p_pjiahyan@tencent.com>
2022-06-21 13:09:12 +0800

    init

--- src/main/java/hm/net/thread/HmExecutor.java
+++ src/main/java/hm/net/thread/HmExecutor.java
@@ -3,9 +3,43 @@ package hm.net.thread;
 /**
  * 执行提交的Runnable任务的对象。该对象提供了一种将任务提交与每个任务如何运行机制解耦的方法，
  * 包括线程的使用，调度等细节。
+ * 通常使用Executor，而不是显式创建线程。
+ * Executor也不是严格的要求线程执行必须是异步的，你也可以如下的立即执行提交的任务
+ *
+ * <pre> {@code
+ * class DirectExecutor implements Executor {
+ *   public void execute(Runnable r) {
+ *     r.run();
+ *   }
+ * }}</pre>
  *
  * @author Yan Jiahong
  * Created on 2022/6/21
  */
-public class HmExecutor {
+public interface HmExecutor {
+
+    /**
+     * 在将来的额某个时间执行给定的指令
+     *
+     * @param command runnable任务
+     * @throws HmRejectedExecutionException 当这个任务无法被接受执行
+     * @throws NullPointerException         如果执行指令为空。
+     */
+    void execute(Runnable command);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/hm/net/thread/HmRejectedExecutionException.java
@@ -0,0 +1,11 @@
+package hm.net.thread;
+
+/**
+ * 被拒绝执行异常。
+ * 当{@link HmExecutor}无法接受任务执行时候抛出该异常。
+ *
+ * @author Yan Jiahong
+ * Created on 2022/6/21
+ */
+public class HmRejectedExecutionException extends RuntimeException {
+}
