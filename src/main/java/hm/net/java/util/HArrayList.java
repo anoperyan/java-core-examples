10dc1385bb21b4180bb2a5e93c9271c4d4170b9d
p_pjiahyan <p_pjiahyan@tencent.com>
2022-06-25 13:12:06 +0800

    init

---
+++ src/main/java/hm/net/java/util/HArrayList.java
@@ -0,0 +1,10 @@
+package hm.net.java.util;
+
+/**
+ * 参考 {@link java.util.ArrayList}
+ *
+ * @author Yan Jiahong
+ * Created on 2022/6/23
+ */
+public class HArrayList {
+}
---
+++ src/main/java/hm/net/java/util/concurrent/HConcurrentHashMap.java
@@ -0,0 +1,10 @@
+package hm.net.java.util.concurrent;
+
+/**
+ * 参考 {@link java.util.concurrent.ConcurrentHashMap}
+ *
+ * @author Yan Jiahong
+ * Created on 2022/6/23
+ */
+public class HConcurrentHashMap {
+}
